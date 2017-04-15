dd229b9c3167e462ca21e6e7847e10507adad3d6
harrietpavey <harriet.pavey@gmail.com>
2017-04-15 08:54:49 +0100

    Add files via upload

---
+++ amb.js
@@ -0,0 +1,7 @@
+function jsDropDown(imgid,newimg){
+	document.getElementById(imgid).src = "C:/Users/harri/OneDrive/Documents/MA Interactive Journalism/Data Journalism/Ambulance/" + newimg + ".png";
+}
+
+function myFunction() {
+    document.getElementById("jsDropDown").classList.toggle("show");
+}
